40203ec296f68c5afe18eafe3bbb5b0ad8bb4d28
Mike Warot <chezmike@gmail.com>
2024-11-10 02:39:28 -0600

    add hint about the context menu

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <h1>Bitgrid - a novel  BITwise GRID-based Computation Model</h1>
         <LEDGrid rows={10} cols={10} />
         <h2><a href="https://github.com/mikewarot/bitgrid-react-app">GitHub repository</a></h2>
+        You can long press (on a smartphone) or right click (on a computer) to see the context menu, and set the programming for a bitgrid cell
       </header>
     </div>
   );
