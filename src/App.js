646daa402f306d9586fbf370081a51319e5ae38a
Mike Warot <chezmike@gmail.com>
2024-11-15 20:07:57 -0600

    Remove message about long press

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
       <header className="App-header">
         <LEDGrid rows={10} cols={10} />
         <h2><a href="https://github.com/mikewarot/bitgrid-react-app">GitHub repository</a></h2>
-        You can long press (on a smartphone) or right click (on a computer) to see the context menu, and set the programming for a bitgrid cell
       </header>
     </div>
   );
